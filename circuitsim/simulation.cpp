c2f6a2c470156754c5a8f48487edad82507bd612
vitor <rotiv.leunam.123@gmail.com>
2016-02-28 15:35:11 +0000

    small JNI refactor to use C++ API

---
+++ circuitsim/simulation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vitor on 27-06-2015.
+//
+
+#include "simulation.h"
---
+++ circuitsim/simulation.h
@@ -0,0 +1,15 @@
+//
+// Created by vitor on 27-06-2015.
+//
+
+#ifndef ROOT_SIMULATION_H
+#define ROOT_SIMULATION_H
+
+
+class simulation
+{
+
+};
+
+
+#endif //ROOT_SIMULATION_H
